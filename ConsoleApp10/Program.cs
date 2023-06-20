28edcb3fbc6b6d17542b76f39d72c18321d2a213
Cab41-11 <Cab41-11@DESKTOP-25SIN69>
2023-06-20 09:08:49 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 7, 9, 8, 9, 10, 11, 12 };
+            int min = arr[0];
+            bool Found = false;
+            for(int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    Found = true;
+                    break;
+                }
+                else if (arr[i] <min)
+                {
+                    min = arr[i];
+                }
+
+            }
+            if(Found)
+            {
+              Console.WriteLine($"Мин значение до первого чётного: {min}");
+            }
+            else 
+            {
+                Console.WriteLine("Чётный элемент не найден");   
+            }
+
+           
+
+            Console.ReadLine();
+        }
+    }
+}
